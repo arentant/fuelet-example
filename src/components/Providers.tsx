7d79af02eaf57f3b42f25797b2cbd49cf093ea5f
Aren <aren@bransfer.io>
2024-12-31 00:51:57 +0400

    yo

---
+++ src/components/Providers.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { defaultConnectors } from '@fuels/connectors';
+import { FuelProvider } from '@fuels/react';
+import { useState } from 'react';
+
+const queryClient = new QueryClient();
+
+const fuelConfig = {
+  connectors: defaultConnectors({
+    devMode: true,
+    // wcProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+  }),
+};
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <button
+        type="submit"
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      >
+        Switch theme {theme}
+      </button>
+
+      <FuelProvider theme={theme} fuelConfig={fuelConfig}>
+        {children}
+      </FuelProvider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
